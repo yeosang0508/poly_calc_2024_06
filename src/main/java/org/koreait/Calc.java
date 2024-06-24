5c48699136ec69308c6d33d51d85df67f0e3ded1
yeosang0508 <yeosang0508@gmail.com>
2024-06-24 20:49:36 +0900

    1 + 1

---
+++ src/main/java/org/koreait/Calc.java
@@ -0,0 +1,13 @@
+package org.koreait;
+
+public class Calc {
+    public static int run(String exp) {
+
+        String[] bits = exp.split(" \\+ ");
+
+        int a = Integer.parseInt(bits[0]);
+        int b = Integer.parseInt(bits[1]);
+
+        return a + b;
+    }
+}
