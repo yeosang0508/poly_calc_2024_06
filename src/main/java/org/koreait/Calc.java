ff06d28ad80ed4606b76468b8f856f86f5dfc530
yeosang0508 <yeosang0508@gmail.com>
2024-06-24 20:52:24 +0900

    2 - 1

--- src/main/java/org/koreait/Calc.java
+++ src/main/java/org/koreait/Calc.java
@@ -3,11 +3,26 @@ package org.koreait;
 public class Calc {
     public static int run(String exp) {
 
-        String[] bits = exp.split(" \\+ ");
+        boolean needToPlus = exp.contains("+");
+        boolean needToMinus = exp.contains("-");
+
+        String[] bits = null;
+
+        if (needToPlus) {
+            bits = exp.split(" \\+ ");
+        } else if (needToMinus) {
+            bits = exp.split(" \\- ");
+        }
 
         int a = Integer.parseInt(bits[0]);
         int b = Integer.parseInt(bits[1]);
 
-        return a + b;
+        if (needToPlus) {
+            return a + b;
+        } else if (needToMinus) {
+            return a - b;
+        }
+
+        throw new RuntimeException("해석 불가 : 올바른 계산식이 아니야");
     }
 }
