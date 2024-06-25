22bef3a541642548d57da187e758a2fe1abf8a2e
yeosang0508 <yeosang0508@gmail.com>
2024-06-25 21:20:47 +0900

    10 * 10 * 10 (another answer)

--- src/main/java/org/koreait/Calc.java
+++ src/main/java/org/koreait/Calc.java
@@ -3,34 +3,34 @@ package org.koreait;
 public class Calc {
 
     public static int run(String exp) {
+
         boolean needToMulti = exp.contains("*");
+        boolean needToPlus = exp.contains("+");
 
-        exp = exp.replaceAll("- ", "+ -");
+        if (needToPlus) {
+            exp = exp.replaceAll("- ", "+ -");
 
-        String[] bits = exp.split(" \\+ ");
+            String[] bits = exp.split(" \\+ ");
 
-        int sum = 0;
+            int sum = 0;
 
-        int num = 1;
-        if (needToMulti) {
-            String[] bits2 = bits[bits.length - 1].split(" \\* ");
-
-            for (int i = 0; i < bits2.length; i++) {
-                num *= Integer.parseInt(bits2[i]);
-            }
-            for (int i = 0; i < bits.length - 1; i++) {
+            for (int i = 0; i < bits.length; i++) {
                 sum += Integer.parseInt(bits[i]);
             }
 
-            return sum + num;
+            return sum;
+
+        } else if (needToMulti) {
+            String[] bits = exp.split(" \\* ");
+            int sum = 1;
+
+
+            for (int i = 0; i < bits.length; i++) {
+                sum *= Integer.parseInt(bits[i]);
+            }
+
+            return sum;
         }
-
-        for (int i = 0; i < bits.length; i++) {
-            sum += Integer.parseInt(bits[i]);
-        }
-
-        return sum;
-
-// throw new RuntimeException("해석 불가 : 올바른 계산식이 아니야");
+        throw new RuntimeException("해석 불가 : 올바른 계산식이 아니야");
     }
 }
