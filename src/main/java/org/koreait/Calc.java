578f2f4ed08d7450f105db21a237b6f867efd5bd
yeosang0508 <yeosang0508@gmail.com>
2024-06-27 10:59:49 +0900

    -(8 + 2) * -(7 + 3) + 5 == 105_my_solution(1)_fail_reason

--- src/main/java/org/koreait/Calc.java
+++ src/main/java/org/koreait/Calc.java
@@ -9,31 +9,12 @@ public class Calc {
     public static int runCallCount = 0;
 
     public static int run(String exp) {
-        runCallCount++;
+        runCallCount++; // 재귀함수 호출될때마다 하나씩 증가
 
+        // 10 + (10 + 5)
         exp = exp.trim(); // 양 옆의 쓸데없는 공백 제거
         // 괄호 제거
         exp = stripOuterBrackets(exp);
-
-        // 만약에 -( 패턴이라면, 내가 갖고있는 코드는 해석할 수 없으므로 해석할 수 있는 형태로 수정
-        if (isCaseMinusBracket(exp)) {
-
-            int in = isCaseMinusBrackets(exp);
-
-            String minusbracke1 = exp.substring(in + 1, in + 8) + " * -1 ";
-            String operator1 = exp.substring(in + 9, in + 10);
-            String minusbracke2 = exp.substring(in + 10 );
-
-
-            exp = Calc.run(minusbracke1) + " " + operator1 + minusbracke2;
-
-            if(exp.contains("-(")){
-                String n = minusbracke1;
-                return Calc.run(exp);
-            }
-
-        }
-
 
         if (debug) {
             System.out.printf("exp(%d) : %s\n", runCallCount, exp);
@@ -44,12 +25,21 @@ public class Calc {
             return Integer.parseInt(exp);
         }
 
-         boolean needToMulti = exp.contains(" * ");
+        boolean needToMulti = exp.contains(" * ");
         boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
         boolean needToSplit = exp.contains("(") || exp.contains(")");
         boolean needToCompound = needToMulti && needToPlus;
 
         if (needToSplit) {
+
+            if(exp.contains("-(")) {
+                exp = exp.replaceAll("-\\(", "(-");
+                exp = exp.replaceAll(" \\+ ", " + -");
+                exp = exp.replaceAll("\\) \\+ -", ") + ");
+                return Calc.run(exp);
+            }
+
+
             int splitPointIndex = findSplitPointIndex(exp);
 
             String firstExp = exp.substring(0, splitPointIndex);
@@ -60,6 +50,7 @@ public class Calc {
             exp = Calc.run(firstExp) + " " + operator + " " + Calc.run(secondExp);
 
             return Calc.run(exp);
+
         } else if (needToCompound) {
             String[] bits = exp.split(" \\+ ");
 
@@ -95,72 +86,27 @@ public class Calc {
         throw new RuntimeException("해석 불가 : 올바른 계산식이 아니야");
     }
 
-    private static int isCaseMinusBrackets(String exp) {
-            int index = CaseMinusBracketsIndex(exp);
-
-        return index;
-    }
-
-    private static int CaseMinusBracketsIndex(String exp) {
-
-        for(int i = 0; i < exp.length(); i++) {
-            if(exp.charAt(i) == '-' && exp.charAt(i+1) == '(') {
-                return i;
-            }
-        }
-       return exp.length();
-    }
-
-
-
-
-
-    private static boolean isCaseMinusBracket(String exp) {
-        // -( 로 시작하는지? ->  포함하는지
-
-        // - 인데스 위치 확인 후 + 1
-        // bracketsCount
-        if (exp.contains("-(") == false) return false;
-
-        // 괄호로 감싸져 있는지?
-        int bracketsCount = 0;
-
-        for (int i = 0; i < exp.length(); i++) {
-            char c = exp.charAt(i);
-
-            if (c == '(') {
-                bracketsCount++;
-            } else if (c == ')') {
-                bracketsCount--;
-            }
-            if (bracketsCount == 0) {
-                if (exp.length() - 1 != i) return true;
-            }
-        }
-        return false;
-    }
-
-
     private static int findSplitPointIndex(String exp) {
-        int index = findSplitPointIndexBy(exp, '+');
+
+        int index = findSplitPointIndexBy(exp, '*');
 
         if (index >= 0) return index;
 
-        return findSplitPointIndexBy(exp, '*');
+        return findSplitPointIndexBy(exp, '+');
     }
 
     private static int findSplitPointIndexBy(String exp, char findChar) {
-        int bracketsCount = 0;
+        int brackesCount = 0;
 
         for (int i = 0; i < exp.length(); i++) {
             char c = exp.charAt(i);
 
             if (c == '(') {
-                bracketsCount++;
+                brackesCount++;
             } else if (c == ')') {
-                bracketsCount--;
+                brackesCount--;
             } else if (c == findChar) {
-                if (bracketsCount == 0) return i;
+                if (brackesCount == 0) return i;
             }
         }
         return -1;
@@ -178,3 +124,11 @@ public class Calc {
         return exp.substring(outerBracketsCount, exp.length() - outerBracketsCount);
     }
 }
+
+
+// 해결책 1로는 -(8 + 2) * -(7 + 3) + 5 라는 모양을 replaceAll을 사용하여 (-8 + -2) * (-7 + -3) + 5라는 모양으로 바꾸어주고,
+// * 에서 나누어서 코딩, ) +에서 나누어서 코딩 해보았지만, +에서 나누었을때는
+// -8 + -2) * (-7 + -3 이런 모양으로 깨져서 나왔지만 ) * ( 을 어떻게 해야 해결할 수 있을까 생각
+//  *에서 나누었을 때는 잘 실행되는 것처럼 보였지만 (-8 + -2) 먼저 계산 후 남아있고,
+// 뒤에 (-7 + -3) + 5 먼저 계산 후 (-8 + -2)과 합쳐지기에 *이 먼저 되야되는 상황이 해결되지 않았다.
+//
